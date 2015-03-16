e58d51493a1688fd26e2c3e2e74244cd4752c187
persephonepunch <yoonsunlee150@gmail.com>
2015-03-16 18:41:49 -0400

    QR-XCODE example
    Working QR code

---
+++ Watch WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Watch WatchKit Extension
+//
+//  Created by YOONSUN LEE on 2/25/15.
+//  Copyright (c) 2015 YOONSUN LEE. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
